859a56a2803d8580b36b92e3c02bdb49d9bbd867
railhead <mschmidt@railheadcorp.com>
2019-03-29 22:22:58 -0500

    added customMux.go

--- main.go
+++ main.go
@@ -2,11 +2,8 @@ package main
 
 import (
 	"encoding/json"
-	"net/http"
-	"net/url"
-	"regexp"
-
 	"github.com/sirupsen/logrus"
+	"net/http"
 )
 
 var (
@@ -90,56 +87,4 @@ func getSecretKeyHandler(context *Context) {
 func closeSecretKeyHandler(context *Context) {
 	closeSecretKey(context.Parameters.Get("secretKey"))
 	context.returnJson(http.StatusOK)
-}
-
-type Handler func(*Context)
-type Route struct {
-	Pattern    *regexp.Regexp
-	Handler    Handler
-	Permission string
-}
-type CustomMux struct {
-	Routes       []Route
-	DefaultRoute Handler
-}
-
-func (customMux *CustomMux) Handle(pattern string, handler Handler, permission string) {
-	regexp := regexp.MustCompile(pattern)
-	route := Route{Pattern: regexp, Handler: handler, Permission: permission}
-
-	customMux.Routes = append(customMux.Routes, route)
-}
-
-func (customMux *CustomMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	context := &Context{Request: r, ResponseWriter: w, ReturnData: make(map[string]interface{})}
-	for _, route := range customMux.Routes {
-		log.Debug(route)
-		if matches := route.Pattern.FindStringSubmatch(context.URL.Path); len(matches) > 0 {
-			context.Parameters = context.URL.Query()
-			secretKey := SecretKey(context.Parameters.Get("secretKey"))
-			if (route.Permission == "NONE") || (secretKey != "" && secretKeyHasPermission(secretKey, route.Permission)) {
-				route.Handler(context)
-			}
-			return
-		}
-	}
-	customMux.DefaultRoute(context)
-}
-
-type Context struct {
-	http.ResponseWriter
-	*http.Request
-	Parameters url.Values
-	ReturnData map[string]interface{}
-}
-
-func (context *Context) returnJson(code int) {
-	context.ResponseWriter.Header().Set("Content-Type", "application/json")
-	context.WriteHeader(code)
-
-	outputDataBytes, err := json.Marshal(context.ReturnData)
-	if err != nil {
-		log.Error(err)
-	}
-	context.ResponseWriter.Write(outputDataBytes)
 }
